4eddeab90dc1591ca709799d647b05fd7974c934
Jamal Meizongo <mrmeizongo@thesleepingwalrus.com>
2022-08-11 23:22:21 -0400

    Minor fixes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./components/App";
 import reportWebVitals from "./analytics/reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
