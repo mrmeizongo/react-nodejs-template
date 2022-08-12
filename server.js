0a3cebdc67a73d314348f2406246744d3452a9da
Jamal Meizongo <mrmeizongo@thesleepingwalrus.com>
2022-08-11 23:00:28 -0400

    Initial working version

---
+++ server.js
@@ -0,0 +1,39 @@
+"use strict";
+
+// Dependencies
+//------------------------------------------------------------
+const express = require("express");
+const path = require("path");
+const { networkInterfaces } = require("os");
+//------------------------------------------------------------
+
+// Constants
+//------------------------------------------------------------
+const PORT = process.env.PORT || 3000;
+const app = express();
+//------------------------------------------------------------
+
+// Middlewares
+//------------------------------------------------------------
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "build")));
+//------------------------------------------------------------
+
+// Routes
+//------------------------------------------------------------
+app.get("/*", (req, res) => {
+	res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+//------------------------------------------------------------
+
+// Server
+//------------------------------------------------------------
+app.listen(PORT, (error) => {
+	if (error) return console.log(`There was an error: ${error}`);
+
+	const address = networkInterfaces().wlo1[0]?.address;
+	console.log(`Server started successfully on https://${address}:${PORT}`);
+});
+
+//------------------------------------------------------------
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./css/App.css";
+
+function App() {
+	return (
+		<div className="app">
+			<h2>Welcome to react-nodejs-template ❤️</h2>
+			<p>Lets write some code</p>
+		</div>
+	);
+}
+
+export default App;
